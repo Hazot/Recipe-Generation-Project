9a31464c4131e1d3c31b6df87e86d7a61c183cf9
Kevin Lessard <kevinlessard74@gmail.com>
2023-12-05 20:52:00 -0500

    small fix

--- run_generation.py
+++ run_generation.py
@@ -1,4 +1,5 @@
 import hydra
+import json
 from omegaconf import DictConfig
 import logging
 
@@ -10,33 +11,44 @@ from generation.generation import generate_recipes
 def main(params: DictConfig):
     # setup basic logging
     logger = logging.getLogger(__name__)
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                        datefmt='%m/%d/%Y %H:%M:%S',
-                        level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
 
-    if params['main']['model_type'] == 'gpt2':
-        params['main'].update(params['gpt2'])
-    elif params['main']['model_type'] == 'opt-125m':
-        params['main'].update(params['opt-125m'])
-    elif params['main']['model_type'] == 'opt-350m':
-        params['main'].update(params['opt-350m'])
-    elif params['main']['model_type'] == 'qlora':
-        params['main'].update(params['qlora'])
+    if params["main"]["model_type"] == "gpt2":
+        params["main"].update(params["gpt2"])
+    elif params["main"]["model_type"] == "opt-125m":
+        params["main"].update(params["opt-125m"])
+    elif params["main"]["model_type"] == "opt-350m":
+        params["main"].update(params["opt-350m"])
+    elif params["main"]["model_type"] == "qlora":
+        params["main"].update(params["qlora"])
     else:
         raise Exception("Unknown model type")
 
     # Set the absolute path to the model, if the model has already been trained
-    params['main']['model_name_or_path'] = hydra.utils.get_original_cwd() + params['main']['model_name_or_path']
+    params["main"]["model_name_or_path"] = (
+        hydra.utils.get_original_cwd() + params["main"]["model_name_or_path"]
+    )
 
-    if not params['main']['evaluate']:
-        logger.info("Generating recipes to print.")
-        logger.info(f"Generating recipes with the following model: {params['main']['model_type']}")
-        logger.info(f"The generation will be done with the following parameters: {params['main']}")
+    if not params["main"]["evaluate"]:
+        logger.info("Generating recipes to print to the cli.")
+        logger.info(
+            f"Generating recipes with the following model: {params['main']['model_type']}"
+        )
+        params_string = json.dumps(dict(params["main"]), indent=4)
+        logger.info(
+            f"The generation will be done with the following parameters: {params_string}"
+        )
         generate_recipes(params=params, logger=logger)
     else:
         logger.info("No generation will be done, the evaluate flag is set to false.")
         generate_finetuned_recipes(params=params, logger=logger)
-        logger.info("Finetuned recipes for evaluation have been successfully generated!")
+        logger.info(
+            "Finetuned recipes for evaluation have been successfully generated!"
+        )
 
     logger.info("Generation successfully finished!")
 
